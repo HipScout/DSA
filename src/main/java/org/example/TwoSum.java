70ef3f9038d9741cc2acf66db243e2085d584e87
HipScout <debjyotid.2706@gmail.com>
2026-03-24 16:39:24 +0530

    first commit

---
+++ src/main/java/org/example/TwoSum.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TwoSum {
+    public static ArrayList<Integer> run(ArrayList<Integer> arrayList, int target)  {
+
+        return new ArrayList<Integer>();
+
+    }
+
+    public static ArrayList<Integer> execute() {
+        ArrayList<Integer> input = new ArrayList<>(10);
+        for (int i=1; i <= 10; i++) {
+            input.add(i);
+        }
+
+        return run(input, 11);
+    }
+}
